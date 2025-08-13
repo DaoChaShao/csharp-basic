5704b67a8c2bb0a7bfe245dea84f0f6dbed04be0
DaoChaShao <27882272@qq.com>
2025-08-13 16:59:03 +0800

    feat: add I.Function project with basic function examples

---
+++ I.Function/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace I.Function;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Function without parameters
+
+        Greet();
+
+        #endregion
+
+        #region Random number setting
+
+        Random random = new Random();
+        int x = random.Next(1, 11);
+        int y = random.Next(1, 11);
+
+        #endregion
+
+        #region Function without return value
+
+        Add(x, y);
+
+        #endregion
+
+        #region Function with return value
+
+        int result = Sum(x, y);
+        Console.WriteLine($"The sum of {x} and {y} is {result}.");
+
+        #endregion
+    }
+
+    private static void Greet()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    private static void Add(int m, int n)
+    {
+        int result = m + n;
+        Console.WriteLine($"The result of {m} plus {n} is {result}.");
+    }
+
+    private static int Sum(int m, int n)
+    {
+        return m + n;
+    }
+}
