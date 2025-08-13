96f60ae7b1d4485a4504a9e9d06f50091b2e47aa
DaoChaShao <27882272@qq.com>
2025-08-13 17:15:48 +0800

    feat: add J.ExceptionHandling project with basic exception handling examples

---
+++ J.ExceptionHandling/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace J.ExceptionHandling;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Random number setting
+
+        Random random = new Random();
+        int dividend = random.Next(1, 11);
+        int divisor = random.Next(0, 2);
+
+        #endregion
+
+        #region Normal Exception
+
+        // try
+        // {
+        //     Console.WriteLine($"The result of {dividend} divided by {divisor} is {dividend / divisor}.");
+        // }
+        // catch (Exception e)
+        // {
+        //     Console.WriteLine(e);
+        //     throw;
+        // }
+        // finally
+        // {
+        //     Console.WriteLine("Goodbye");
+        // }
+
+        #endregion
+
+        #region Typed Exception
+
+        try
+        {
+            Console.WriteLine($"The result of {dividend} divided by {divisor} is {dividend / divisor}.");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero.");
+        }
+        finally
+        {
+            Console.WriteLine("Goodbye");
+        }
+
+        #endregion
+    }
+}
