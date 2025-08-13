02d1bcc4a941a4f0f03c618210b3b0d47f5a8a9d
DaoChaShao <27882272@qq.com>
2025-08-13 15:09:49 +0800

    feat: add E.Basic.Input project with console input handling examples

---
+++ E.Basic.Input/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace E.Basic.Input;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Input with any prompt
+
+        var input = Console.ReadLine();
+        Console.WriteLine($"You entered: {input}");
+
+        #endregion
+
+        #region Input without none prompt
+
+        var enteredText = Console.ReadLine()!;
+        Console.WriteLine($"You entered: {enteredText}");
+
+        #endregion
+
+        #region Input number
+
+        var number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"You entered: {number}");
+
+        #endregion
+    }
+}
