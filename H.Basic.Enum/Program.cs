8773e417e5e7f099cc30625823009eda65f948c4
DaoChaShao <27882272@qq.com>
2025-08-13 16:38:03 +0800

    feat: add H.Basic.Enum project with enum usage examples

---
+++ H.Basic.Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace H.Basic.Enum;
+
+using System;
+
+enum Gender
+{
+    Male,
+    Female,
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Normal Enum Usage
+
+        Console.WriteLine($"The gender is {Gender.Male}");
+        Console.WriteLine($"The gender is {Gender.Female}");
+
+        #endregion
+
+        #region Convert Enum
+
+        int valueMale = (int)Gender.Male;
+        int valueFemale = (int)Gender.Female;
+        Console.WriteLine($"The value of male is {valueMale}");
+        Console.WriteLine($"The value of female is {valueFemale}");
+
+        #endregion
+    }
+}
