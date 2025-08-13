e45a0b867453532b4fbe876f45dca5b70915db83
DaoChaShao <27882272@qq.com>
2025-08-13 15:06:35 +0800

    feat: add C.Basic.Struct project with Person struct and Direction enum for .NET 8.0

---
+++ C.Basic.Struct/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace C.Basic.Struct;
+
+using System;
+
+struct Person
+{
+    public string Name;
+    public string Gender;
+    public uint Age;
+}
+
+enum Direction
+{
+    North,
+    East,
+    South,
+    West
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person tom = new Person();
+        tom.Name = "Tom";
+        tom.Gender = "Male";
+        tom.Age = 20;
+        Console.WriteLine(
+            $"The person's name is {tom.Name}, and the gender is {tom.Gender}, and the age is {tom.Age}."
+            + $"The person is facing {Direction.North}."
+        );
+    }
+}
