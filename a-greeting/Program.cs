e40f2f2b0bb8d085ff5855cf1dd79270c5f730bb
DaoChaShao <27882272@qq.com>
2025-08-13 13:48:28 +0800

    feat: enhance Program.cs with structured regions and variable examples

--- a-greeting/Program.cs
+++ a-greeting/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        #region hello world
+
         Console.WriteLine("Hello, World!");
+
+        #endregion
+
+        #region variables
+
+        var greeting = "Hello, World!";
+        Console.WriteLine(greeting);
+
+        #endregion
+
+        #region string concatenation
+
+        var word = "world";
+        Console.WriteLine($"Hello, {word}!");
+
+        #endregion
     }
 }
