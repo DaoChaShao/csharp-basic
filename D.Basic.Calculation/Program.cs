e858263232effd96020e01d4f24bc44e8cd8aee0
DaoChaShao <27882272@qq.com>
2025-08-13 15:08:27 +0800

    feat: add D.Basic.Calculation project with random number generation and fake data using Bogus

---
+++ D.Basic.Calculation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace D.Basic.Calculation;
+
+using System;
+using Bogus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region FakeData
+
+        var faker = new Faker("en");
+        Console.WriteLine(faker.Name.FullName());
+
+        #endregion
+
+        #region Calculator
+
+        Random random = new Random();
+        var num = random.Next(1, 11);
+        Console.WriteLine($"The random number is {num}.");
+
+        var x = random.Next(1, 11);
+        var y = random.Next(1, 11);
+        Console.WriteLine($"X is {x}, and Y is {y}, and the sum is {x + y}.");
+        Console.WriteLine($"X is {x}, and Y is {y}, and the difference is {x - y}.");
+        Console.WriteLine($"X is {x}, and Y is {y}, and the product is {x * y}.");
+        Console.WriteLine($"X is {x}, and Y is {y}, and the quotient is {x / y}.");
+
+        #endregion
+    }
+}
