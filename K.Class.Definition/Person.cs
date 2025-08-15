b48b846da26f15f83563a321e20fd7a308adb58c
DaoChaShao <27882272@qq.com>
2025-08-15 14:16:31 +0800

    feat: enhance Person class with static person count and virtual Introduce method

--- K.Class.Definition/Person.cs
+++ K.Class.Definition/Person.cs
@@ -1,14 +1,25 @@
+using Bogus.DataSets;
+
 namespace K.Class.Definition;
 
 internal class Person(string name, int age, Gender gender)
 {
-    private string Name { get; set; } = name;
+    private static int _personCount = 0;
+    protected string Name { get; set; } = name;
     private int Age { get; set; } = age;
     private Gender Gender { get; set; } = gender;
 
+    private readonly int _ = (_personCount++, 0).Item2;
 
-    internal string Introduce()
-    {
-        return $"Hello, my name is {Name} and I am {Age} years old, and my gender is {Gender}.";
-    }
+    // Method I
+    // internal string Introduce()
+    // {
+    //     return $"Hello, my name is {Name} and I am {Age} years old, and my gender is {Gender}.";
+    // }
+
+    // Method II
+    internal virtual string Introduce() =>
+        $"Hello, my name is {Name} and I am {Age} years old, and my gender is {Gender}.";
+
+    internal static int GetPersonCount() => _personCount;
 }
