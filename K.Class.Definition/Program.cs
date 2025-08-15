e5c2f61062574bb21d2784750c4f8e1271818f45
DaoChaShao <27882272@qq.com>
2025-08-15 14:16:38 +0800

    feat: enhance object creation logic for Person, Student, and Teacher classes

--- K.Class.Definition/Program.cs
+++ K.Class.Definition/Program.cs
@@ -54,18 +54,58 @@ class Program
 
         #region Create objects in a loop
 
-        int amount = random.Next(3, 6);
-        Console.WriteLine($"The amount of people to create is {amount}.");
+        int amountPerson = random.Next(3, 6);
+        Console.WriteLine($"The amount of people to create is {amountPerson}.");
 
-        for (int i = 1; i <= amount; i++)
+        for (int i = 1; i <= amountPerson; i++)
         {
             // Set random gender
             // Method II: Use the Helper class directly
-            Gender genderRandom = new Helper().GenderPicker();
+            Gender genderPerson = new Helper().GenderPicker();
 
-            Person person = new Person(faker.Name.FullName(), random.Next(6, 24), genderRandom);
+            Person person = new Person(faker.Name.FullName(), random.Next(6, 24), genderPerson);
             Console.WriteLine($"Person {i}: {person.Introduce()}");
         }
+
+        Console.WriteLine($"There are {Person.GetPersonCount()} people created in total.");
+
+        #endregion
+
+        #region Heritance using Student class
+
+        int amountStudent = random.Next(3, 6);
+        Console.WriteLine($"The amount of students to create is {amountStudent}.");
+
+        for (int i = 0; i < amountStudent; i++)
+        {
+            Gender genderStudent = new Helper().GenderPicker();
+
+            Student student = new Student(faker.Name.FullName(), random.Next(6, 24), genderStudent,
+                random.Next(50, 100));
+            Console.WriteLine($"Student {i + 1}: {student.Introduce()}");
+            Console.WriteLine(student.Study());
+        }
+
+        Console.WriteLine($"There are {Person.GetPersonCount()} people created in total.");
+
+        #endregion
+
+        #region Heritance using Teacher class
+
+        int amountTeacher = random.Next(3, 6);
+        Console.WriteLine($"The amount of teachers to create is {amountTeacher}.");
+
+        for (int i = 0; i < amountTeacher; i++)
+        {
+            Gender genderTeacher = new Helper().GenderPicker();
+
+            Teacher teacher = new Teacher(faker.Name.FullName(), random.Next(20, 60), genderTeacher,
+                random.Next(5000, 10000));
+            Console.WriteLine($"Teacher {i + 1}: {teacher.Introduce()}");
+            Console.WriteLine(teacher.Teach());
+        }
+
+        Console.WriteLine($"There are {Person.GetPersonCount()} people created in total.");
 
         #endregion
     }
