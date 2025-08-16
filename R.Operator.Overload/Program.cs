36c5e43266ca52a8417db2faf2b82a6d9b96c6aa
DaoChaShao <27882272@qq.com>
2025-08-16 15:13:04 +0800

    feat: implement main program to demonstrate Box operator overloading

---
+++ R.Operator.Overload/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace R.Operator.Overload;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Initialisation
+
+        var random = new Random();
+
+        #endregion
+
+        #region Box operator overload
+
+        var boxLeft = new Box(random.Next(1, 10), random.Next(1, 10), random.Next(1, 10));
+        var boxRight = new Box(random.Next(1, 10), random.Next(1, 10), random.Next(1, 10));
+        var box = boxLeft + boxRight;
+
+        Console.WriteLine($"Box Left: {boxLeft}");
+        Console.WriteLine($"Box Right: {boxRight}");
+        Console.WriteLine($"Box: {box}");
+
+        #endregion
+    }
+}
