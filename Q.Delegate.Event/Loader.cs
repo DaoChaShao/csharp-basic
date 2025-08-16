ff8f2ca75ebc4c1fbac8ba45fdb99029d0f2c1fe
DaoChaShao <27882272@qq.com>
2025-08-16 14:42:25 +0800

    feat: add Loader class for file, map, and skin loading functionality

---
+++ Q.Delegate.Event/Loader.cs
@@ -0,0 +1,8 @@
+namespace Q.Delegate.Event;
+
+public class Loader
+{
+    internal string LoadFiles(string userName) => $"Hello, {userName}! The files are loading...";
+    internal string LoadMaps(string userName) => $"Hello, {userName}! The maps are loading...";
+    internal string LoadSkins(string userName) => $"Hello, {userName}! The skins are loading...";
+}
